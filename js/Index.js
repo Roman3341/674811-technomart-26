7bcd19167cb07ad387bd900f5e9d185b7a3bfbeb
Roman <fearik435@gmail.com>
2019-10-28 19:16:40 +0300

    Индекс. Модалки. Переключение сервисов

--- js/Index.js
+++ js/Index.js
@@ -1,14 +1,20 @@
 const feautersList = document.querySelectorAll('.feauters-list-link');
-console.log(feautersList);
 for (let i = 0; i < feautersList.length; i++) {
     feautersList[i].addEventListener('click', function(evt) {
         evt.preventDefault();
         
         const target = evt.target;
-        // console.log(target);
+        const activeElement = document.querySelector('.feauters-discription-item-active');
+        const activeLink = document.querySelector('.feauters-list-link-active');
+
+        if (activeLink !== target) {
+        activeElement.classList.remove('feauters-discription-item-active');
+        activeLink.classList.remove('feauters-list-link-active');
+        }
         const dataText = target.dataset.link;
-        // console.log(dataText);
         const detailText = document.querySelector(`#${dataText}`);
-        console.log(detailText);
+        
+        target.classList.add('feauters-list-link-active');
+        detailText.classList.add('feauters-discription-item-active');
     })
 }
