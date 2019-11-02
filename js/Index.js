77e570ca9a182f3b9c3a71af95d1856d68a06994
Roman <fearik435@gmail.com>
2019-11-02 15:32:31 +0300

    правки после 1-ой проверки
    не проработал кретерий Д25.
    Нет глобальных стилей тегов.
    Не делал минификацию CSS и JS, но инструмент нашел

--- js/Index.js
+++ js/Index.js
@@ -1,3 +1,4 @@
+/*слайдер сервисов*/
 const feautersList = document.querySelectorAll('.feauters-list-link');
 for (let i = 0; i < feautersList.length; i++) {
   feautersList[i].addEventListener('click', function (evt) {
@@ -11,33 +12,47 @@ for (let i = 0; i < feautersList.length; i++) {
       activeElement.classList.remove('feauters-discription-item-active');
       activeLink.classList.remove('feauters-list-link-active');
     }
+
     const dataText = target.dataset.link;
     const detailText = document.querySelector(`#${dataText}`);
 
     target.classList.add('feauters-list-link-active');
     detailText.classList.add('feauters-discription-item-active');
 
-
-
-
-
   })
 }
 
+const sliderLeft = document.querySelector('.slider-item-first');
+const sliderRight = document.querySelector('.slider-item-second');
+const sliderButtonRight = document.querySelector('.slider-button-right');
+const sliderButtonLeft = document.querySelector('.slider-button-left');
+
+sliderButtonRight.addEventListener('click', function(evt){
+ sliderLeft.classList.remove('slider-show'); 
+ sliderRight.classList.add('slider-show');
+})
+
+sliderButtonLeft.addEventListener('click', function(evt){
+  sliderRight.classList.remove('slider-show'); 
+  sliderLeft.classList.add('slider-show');
+ })
+ 
+
+/*модальное окно обратной связи*/
 const formBack = document.querySelector('.form-back');
 const writeUs = document.querySelector('.write-us');
 writeUs.addEventListener('click', function (evt) {
   evt.preventDefault();
   formBack.classList.add('modal-show');
 })
-
+/*Модальное окно карты*/
 const map = document.querySelector('.map');
 const mapShow = document.querySelector('.map-modal');
 map.addEventListener('click', function (evt) {
   evt.preventDefault();
   mapShow.classList.add('modal-show');
 })
-
+/*модальное окно "Товар добавлен в корзину"*/
 const cartButton = document.querySelectorAll('.buy-button');
 const cart = document.querySelector('.cart-modal');
 for (let j = 0; j < cartButton.length; j++) {
@@ -46,7 +61,7 @@ for (let j = 0; j < cartButton.length; j++) {
     cart.classList.add('modal-show');
   })
 }
-
+/*Закрытие формы по крестику*/
 const closeButton = document.querySelectorAll('.close-button');
 const continueButton = document.querySelector('.cart-modal-continue');
 const activeModal = document.querySelector('.modal-show');
@@ -62,3 +77,5 @@ for (let e = 0; e < closeButton.length; e++) {
     mapShow.classList.remove('modal-show');
   })
 }
+
+/*Слайдер переключения изображений*/
